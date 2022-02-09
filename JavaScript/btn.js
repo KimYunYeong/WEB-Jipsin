ca9021ffbbd5500b14bb2d05642035d89ab6308c
KimYunYeong <61217259+KimYunYeong@users.noreply.github.com>
2022-02-09 15:03:36 +0900

    Add files via upload

---
+++ JavaScript/btn.js
@@ -0,0 +1,10 @@
+function mOver(obj, clr, bgclr){/*mOver(this, '글자색', '배경색')*/
+  var os = obj.style;
+  os.backgroundColor = bgclr;
+  os.color = clr;
+}
+function mOut(obj, clr, bgclr){/*mOut(this, '글자색', '배경색')*/
+  var os = obj.style;
+  os.backgroundColor = bgclr;
+  os.color = clr;
+}
